9b21409b2131d6d39d098cc3d0cc8658497eb7a9
Vladimir Herdman <vovaherdman@gmail.com>
2025-10-30 10:52:27 -0400

    Initial pywebview UI frontend with API point to call backend functions
    Im leaning this way over tkinter mostly because web based fronted uses customization tools like html and css which were already used to, over trying to learn another python library. This one is pretty simple, its just more frontend work.

--- src/app/ui/ui.py
+++ src/app/ui/ui.py
@@ -1,80 +1,49 @@
-import customtkinter as ctk
-import tkinter.filedialog as tkfd
+import threading
+import webview
 
-class TopBarFrame(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-
-class ButtonRowFrame(ctk.CTkFrame):
-    def __init__(self, master, get_files_func, destination_files_func, run_program_func):
-        super().__init__(master)
-
-        self.grid_columnconfigure([0,1,2], weight=1)
-        self.grid_rowconfigure(0, weight=1)
-
-        self.add_files_button = ctk.CTkButton(self, text="Select Input Files", command=get_files_func)
-        self.add_files_button.grid(row=0, column=0, padx=10, sticky="wne")
-
-        self.add_files_button = ctk.CTkButton(self, text="Select Destination", command=destination_files_func)
-        self.add_files_button.grid(row=0, column=1, padx=10, sticky="wne")
-
-        self.add_files_button = ctk.CTkButton(self, text="Run Program", command=run_program_func)
-        self.add_files_button.grid(row=0, column=2, padx=10, sticky="wne")
-
-class App(ctk.CTk):
+class API:
+    """This is the communication bridge between js and python, it groups the functions
+    we use in the frontend window so it"s easier to call python
+    """
     def __init__(self):
-        super().__init__()
+        self.files = tuple()
+        self.dir_path: str = ""
 
-        #variables to be set throughout app when buttons used
-        self.save_path = ""
-        self.files_to_aggregate = tuple()
+    def get_file_paths(self):
+        return self.files
 
-        #create UI
-        self.title("Scan PBR into Documents (UI IN DEV)")
-        self.geometry("600x400")
-        self.focus_force()
-        self.grid_columnconfigure(0, weight=1) #use this to make widgets stretch
-        self.grid_rowconfigure(1, weight=1)
+    def get_dir_path(self):
+        return self.dir_path
 
-        self.bind("<Escape>", lambda event: self.quit_app())
+    def open_file_dialog(self):
+        #can't have two widnows at once from same thread, so thread webview dialogs
+        def _open_file_dialog():
+            filepaths = webview.windows[0].create_file_dialog(
+                webview.FileDialog.OPEN,
+                allow_multiple=True,
+                file_types=("PDF File (*.pdf)", "Word Doc (*.docx)")
+            )
+            print(filepaths) #REMOVE
+            if (filepaths != None): self.files = filepaths
+        threading.Thread(target=_open_file_dialog).start()
 
-        self.top_bar = TopBarFrame(self)
-        self.top_bar.grid(row=0, column=0, sticky="wne")
+    def open_directory_dialog(self):
+        def _open_directory_dialog():
+            dir_path = webview.windows[0].create_file_dialog(
+                webview.FileDialog.FOLDER,
+            )
+            print(dir_path) #REMOVE
+            if (dir_path != None): self.dir_path = dir_path[0]
+        threading.Thread(target=_open_directory_dialog).start()
 
-        self.button_row = ButtonRowFrame(self, self.get_files_func, self.destination_files_func, self.run_program_func)
-        self.button_row.grid(row=1, column=0, sticky="nesw")
+    def print(self, val, identifier):
+        print(f"Value from {identifier}:{val}")
 
-    def quit_app(self):
-        self.destroy()
-
-    def get_files_func(self):
-        print("get files clicked") #REMOVE
-        filepaths = tkfd.askopenfilenames(
-            title="Choose pdf files to aggregate",
-            filetypes=[("PDF File", "*.pdf"), ("Word Doc", "*.docx")],
-            initialdir=None
-        )
-        print(filepaths) #REMOVE
-        if (filepaths != None): self.files_to_aggregate = filepaths
-        self.focus_force()
-
-    def destination_files_func(self):
-        print("destination files clicked") #REMOVE
-        save_path = tkfd.askdirectory(
-            initialdir=None,
-            mustexist=True,
-            parent=None,
-            title="Destination to save files to"
-        )
-        print(save_path) #REMOVE
-        if (save_path != None): self.save_path = save_path
-        self.focus_force()
-
-    def run_program_func(self):
-        print("run program clicked") #REMOVE
-
-#this only runs if this file is directly ran, not imported
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
-
+    api = API()
+    webview.create_window(
+        title="Hello world",
+        url="./index.html",
+        js_api=api,
+    )
+    webview.start()
